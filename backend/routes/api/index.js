b863b4129eec1c6849319b29216cfbfe8f20b925
Savannah Rice <savannah.rice@gmail.com>
2021-03-25 10:36:50 -0600

    Finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,44 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+
+
 
 router.post('/test', function(req, res){
     res.json({requestBody: req.body});
 });
+
+//Code for testing middleware
+
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//       return res.json(req.user);
+//     }
+//   );
+
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//           username: 'Demo-lition'
+//         },
+//       })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   }));
+
 
 module.exports = router;

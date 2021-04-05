7b6b80602c94b6a253405023d388fa7989bfa09a
Savannah Rice <savannah.rice@gmail.com>
2021-04-05 08:59:01 -0600

    Modified edit page

--- frontend/src/components/SingleHomePage/review.js
+++ frontend/src/components/SingleHomePage/review.js
@@ -101,49 +101,52 @@ const Reviews = () => {
                 })
                }
             </ul>
-            <form onSubmit={handleReviewSubmit}></form>
-                <label >Edit Your Review!</label>
-                <input
-                className='input'
-                type='textarea' 
-                required
-                multiline
-                value={description}
-                placeholder='your review text'
-                onChange={(e) => setDescription(e.target.value)}
-                />
-                <input 
-                type="radio" 
-                value='1'
-                onChange={(e) => setRating(e.target.value)}
-                checked={rating === '1'}
-                />1 ⭐
-                <input 
-                type="radio" 
-                onChange={(e) => setRating(e.target.value)}
-                checked={rating === '2'}
-                value='2'
-                />2 ⭐️
-                <input 
-                type="radio" 
-                value='3'
-                onChange={(e) => setRating(e.target.value)}
-                checked={rating === '3'}
-                />3 ⭐️
-                <input 
-                type="radio" 
-                value='4'
-                onChange={(e) => setRating(e.target.value)}
-                checked={rating === '4'}
-                />4 ⭐️
-                <input 
-                type="radio" 
-                value='5'
-                onChange={(e) => setRating(e.target.value)}
-                checked={rating === '5'}
-                />5 ⭐️
-                <button type='submit' onClick={handleEditSubmit}>Change Rating!</button>
-                <button type='submit' onClick={handleCancelEdit}>Cancel</button>
+            <div className='entireReviewDiv'>
+                <form onSubmit={handleReviewSubmit}></form>
+                    <label >Add Your Review!</label>
+                    <input type="text area"
+                    className='input'
+                    required
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+                    <div className='ratingsButtons'>
+                        <input
+                        type="radio"
+                        value='1'
+                        onChange={(e) => setRating(e.target.value)}
+                        checked={rating === '1'}
+                        />1 ⭐️
+                        <input
+                        type="radio"
+                        onChange={(e) => setRating(e.target.value)}
+                        checked={rating === '2'}
+                        value='2'
+                        />2 ⭐️
+                        <input
+                        type="radio"
+                        value='3'
+                        onChange={(e) => setRating(e.target.value)}
+                        checked={rating === '3'}
+                        />3 ⭐️
+                        <input
+                        type="radio"
+                        value='4'
+                        onChange={(e) => setRating(e.target.value)}
+                        checked={rating === '4'}
+                        />4 ⭐️
+                        <input
+                        type="radio"
+                        value='5'
+                        onChange={(e) => setRating(e.target.value)}
+                        checked={rating === '5'}
+                        />5 ⭐️
+                        <div className='editButtonDiv'>
+                            <button className='reviewButtonEdit' type='submit' onClick={handleEditSubmit}>Change Rating!</button>
+                            <button className='reviewButtonEdit' type='submit' onClick={handleCancelEdit}>Cancel</button>
+                        </div>
+                    </div>
+            </div>
         </div>
     )
         
